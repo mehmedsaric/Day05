dbf988df97d63a14600019077ce0a780b97e00e8
mehmedsaric <mehmed.saaric@gmail.com>
2021-03-13 01:05:18 +0100

    obradjen

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,7 +2,7 @@ function main(){
 var myString = "Concatenation is the process of appending ";
 
 myString += "one string to the end of another string.";
-return myString
+return myString;
 
  }
  console.log(main());
