f92d5e33a61a9e94a23ad8707c73f5deac530e54
mehmedsaric <mehmed.saaric@gmail.com>
2021-03-13 01:20:01 +0100

    obradjen

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(){ 
+    var lastName = "Doe";
+    var firstLetterOfLastName = lastName[0];
+    
+    lastName.length;
+    return firstLetterOfLastName;
+    
+    }
+    console.log(main());
+    module.exports = main;
