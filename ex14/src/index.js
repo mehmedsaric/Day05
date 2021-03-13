3b610a39aa7ee82ca46bb236d40662fa88556dcb
mehmedsaric <mehmed.saaric@gmail.com>
2021-03-13 01:36:20 +0100

    obradjen

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main(){ 
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+
+var worldBanks = "My " + myNoun + " loves to " + myVerb + " as " + myAdjective + " and " + myAdverb + ".";
+return worldBanks;
+
+
+}
+console.log(main());
+module.exports = main;
